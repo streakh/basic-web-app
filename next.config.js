265602f6167cb7e38a375c2bab03b76f3968fd86
Streak <ahoney@andrew.cmu.edu>
2025-10-07 10:03:33 -0400

    undid api rerouting

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/API',
-        destination: '/api',
-      },
-    ];
-  },
 }
 
 module.exports = nextConfig
